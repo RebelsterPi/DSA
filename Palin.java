848f90216509db093546fc43c2a990a4be92eb00
Pranesh Ulleri <u4cse17254@11cpu0277L.amritavidya.edu>
2018-09-17 14:44:08 +0530

    DSA

---
+++ Palin.java
@@ -0,0 +1,53 @@
+package lab;
+
+public class Palin {
+	class Node
+	{
+	    String data;
+	    Node next;
+	 
+	    Node(String d)
+	    {
+	        data = d;
+	        next = null;
+	    }
+	}
+	 
+	class Palindrome
+	{
+	    Node head;
+	 
+	      boolean isPalidromeUtil(String str)
+	    {
+	        int length = str.length();
+	 
+	           for (int i=0; i<length/2; i++)
+	            if (str.charAt(i) != str.charAt(length-i-1))
+	                return false;
+	 
+	        return true;
+	    }
+	 
+	     boolean isPalindrome()
+	    {
+	        Node node = head;
+	        String str = "";
+	        while (node != null)
+	        {
+	            str = str.concat(node.data);
+	            node = node.next;
+	        }
+	        return isPalidromeUtil(str);
+	    }
+	 
+	     public void main(String[] args)
+	    {
+	        Palindrome list = new Palindrome();
+	        list.head = new Node("112");
+	        list.head.next=new Node("11");
+	        System.out.println(list.isPalindrome());
+	 
+	    } }  }
+	
+
+
